e1eda49691e817516b611966122878b5486ccc78
lucianovsjr <lucianovsjr@gmail.com>
2023-06-18 15:01:49 -0300

    Desregistrando eventos

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -17,11 +17,16 @@ export default class EventDispatcher implements EventDispatcherInterface {
     }
 
     unregister(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
-
+        if (this.eventHandlers[eventName]) {
+            const index = this.eventHandlers[eventName].indexOf(eventHandler);
+            if (index >= 0) {
+                this.eventHandlers[eventName].splice(index, 1);
+            }
+        }
     }
 
     unregisterAll(): void {
-
+        this.eventHandlers = {};
     }
 
     notify(event: EventInterface): void {
