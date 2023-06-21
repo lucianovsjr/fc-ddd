6b729bd6fce4a2ba1da33143ecc82860611ead28
lucianovsjr <lucianovsjr@gmail.com>
2023-06-21 00:05:52 -0300

    Modularizando a camada de infraestrutura

--- src/infrastructure/checkout/repository/sequilize/order.model.ts
+++ src/infrastructure/checkout/repository/sequilize/order.model.ts
@@ -7,7 +7,7 @@ import {
     BelongsTo,
     HasMany,
 } from "sequelize-typescript";
-import CustomerModel from "./customer.model";
+import CustomerModel from "../../../customer/repository/sequilize/customer.model";
 import OrderItemModel from "./order-item.model";
 
 @Table({
--- src/infrastructure/checkout/repository/sequilize/order.repository.spec.ts
+++ src/infrastructure/checkout/repository/sequilize/order.repository.spec.ts
@@ -1,15 +1,15 @@
 import { Sequelize } from "sequelize-typescript";
-import Customer from "../../domain/customer/entity/customer";
-import Address from "../../domain/customer/value-object/address";
-import CustomerModel from "../db/sequelize/model/customer.model";
-import CustomerRepository from "./customer.repository";
-import OrderModel from "../db/sequelize/model/order.model";
-import OrderItemModel from "../db/sequelize/model/order-item.model";
-import ProductModel from "../db/sequelize/model/product.model";
-import ProductRepository from "./product.repository";
-import Product from "../../domain/product/entity/product";
-import OrderItem from "../../domain/checkout/entity/orderItem";
-import Order from "../../domain/checkout/entity/order";
+import Customer from "../../../../domain/customer/entity/customer";
+import Address from "../../../../domain/customer/value-object/address";
+import CustomerModel from "../../../customer/repository/sequilize/customer.model";
+import CustomerRepository from "../../../customer/repository/sequilize/customer.repository";
+import OrderModel from "./order.model";
+import OrderItemModel from "./order-item.model";
+import ProductModel from "../../../product/repository/sequilize/product.model";
+import ProductRepository from "../../../product/repository/sequilize/product.repository";
+import Product from "../../../../domain/product/entity/product";
+import OrderItem from "../../../../domain/checkout/entity/orderItem";
+import Order from "../../../../domain/checkout/entity/order";
 import OrderRepository from "./order.repository";
 
 describe("Order repository test", () => {
